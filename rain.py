476c43ce8611a2583842d6db5c08ce963a0b03be
David <David@sunnyburrow.com>
2023-12-05 16:05:03 -0500

    rain aggregation of 0 is none, not 0, fixed that

--- rain.py
+++ rain.py
@@ -12,10 +12,10 @@ def raincondition():
                 where from_unixtime(dt) < now() order by dt desc LIMIT 48 ) as t1"""
         db.conn_local_prod_cursor.execute(query)
         amount = db.conn_local_prod_cursor.fetchall()
-        if amount[0][0] > 0.1:
+        if amount[0][0] > 0.1 and amount[0][0] != 'None':
             rainfall = "In the past 48 hours, we've had {0} inches of rainfall".format(amount[0][0])
     except Exception as e:
-        er.add_events("ERROR: Issue pulling past 2 days of rainfall: {}")
+        er.add_events("ERROR: Issue pulling past 2 days of rainfall: {}".format(e))
     return rainfall
 
 if __name__ == "__main__":
